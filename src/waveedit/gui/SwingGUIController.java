717a6b9a4bf330b39efafafc14b9b7d612622da9
Gerogeologie <georgeologie@web.de>
2018-04-07 23:47:39 +0200

    Buttons added to GUI

--- src/waveedit/gui/SwingGUIController.java
+++ src/waveedit/gui/SwingGUIController.java
@@ -1,45 +1,44 @@
 package waveedit.gui;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.event.WindowAdapter;
 
 import javax.swing.JFrame;
 
 public class SwingGUIController implements IGuiController {
-    Graphics2D g;
-    JFrame frame = new JFrame();
-    SwingGUIMap map;
+	Graphics2D g;
+	JFrame frame = new JFrame();
+	SwingGUIMap map;
 
-    public SwingGUIController() {
+	public SwingGUIController() {
 
-    }
+	}
 
-    @Override
-    public void initialize() {
-        // Fenster öffnen
-        frame.setTitle("WaveEdit");
-        frame.setSize(800, 600);
-        // TODO Hintergrund zeichnen
-        frame.getContentPane().setBackground(new Color(250, 230, 200));
-        frame.setResizable(false);
-        frame.setVisible(true);
+	@Override
+	public void initialize() {
+		// TODO open Frame
+		frame.setTitle("WaveEdit");
+		frame.setSize(800, 600);
+		// TODO Draw Background
+		frame.setResizable(false);
+		frame.setVisible(true);
 
-        frame.addWindowListener(new WindowAdapter() {
+		frame.addWindowListener(new WindowAdapter() {
 
-        });
-        // TODO Karte zeichnen
-        map = new SwingGUIMap();
-        frame.getContentPane().add(map);
-        // TODO Menü zeichnen
-    }
+		});
+		// TODO Karte zeichnen
+		map = new SwingGUIMap();
 
-    // protected void paint() {
-    // super.paintComponent(g);
-    // g.setColor (Color.red);
-    // g.fillRect(500,400,50,50);
-    //
-    //
-    // }
+		frame.getContentPane().add(map);
+		// TODO Menü zeichnen
+	}
+
+	// protected void paint() {
+	// super.paintComponent(g);
+	// g.setColor (Color.red);
+	// g.fillRect(500,400,50,50);
+	//
+	//
+	// }
 
 }
--- src/waveedit/gui/SwingGUIMap.java
+++ src/waveedit/gui/SwingGUIMap.java
@@ -1,17 +1,72 @@
 package waveedit.gui;
 
-import javax.swing.JRootPane;
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+import javax.swing.JButton;
 
-public class SwingGUIMap extends JRootPane {
+public class SwingGUIMap extends JPanel {
+	//TODO draw GUI
+	SwingGUIController ctrl;
+	SwingGUIMap map;
+	Graphics g;
 
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 1922691013005376966L;
+	private static final long serialVersionUID = 1922691013005376966L;
 
-    public SwingGUIMap() {
-        // TODO Auto-generated constructor stub
+	public SwingGUIMap() {
 
-    }
+		JButton Save = new JButton("SAVE");
+		JButton EMPTY = new JButton("Empty");
+		JButton Wall = new JButton("Wall");
+		JButton Nexus = new JButton("Nexus");
+		JButton Delete = new JButton("Delete");
+		setLayout(null);
+
+		Save.setBounds(670, 450, 100, 20);
+		setBounds(0, 0, 800, 600);
+		Save.setVisible(true);
+		add(Save);
+
+		EMPTY.setBounds(675, 60, 90, 30);
+		setBounds(0, 0, 800, 600);
+		EMPTY.setVisible(true);
+		add(EMPTY);
+		
+		Wall.setBounds(675, 100, 90, 30);
+		setBounds(0, 0, 800, 600);
+		Wall.setVisible(true);
+		add(Wall);
+		
+		Nexus.setBounds(675, 140, 90, 30);
+		setBounds(0, 0, 800, 600);
+		Nexus.setVisible(true);
+		add(Nexus);
+		
+		Delete.setBounds(675, 180, 90, 30);
+		setBounds(0, 0, 800, 600);
+		Delete.setVisible(true);
+		add(Delete);
+
+	}
+
+	public void paint(Graphics g) {
+
+		super.paintComponent(g);
+		g.setColor(new Color(150, 150, 150));
+		g.fillRect(50, 50, 600, 350);
+		g.fillRect(670, 50, 100, 200);
+
+		// File field
+		g.setColor(Color.white);
+		g.fillRect(50, 450, 600, 20);
+		// Borders
+		g.setColor(Color.black);
+		g.drawRect(50, 450, 600, 20);
+		g.drawRect(670, 50, 100, 200);
+		g.drawRect(50, 50, 600, 350);
+		setBackground(new Color(250, 230, 200));
+
+		setVisible(true);
+	}
 
 }
