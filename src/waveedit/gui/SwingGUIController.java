856b17f51fa0dd3b1035b7400508099ab3c30c7b
smoenig <smoenig@tzi.de>
2018-04-07 11:21:41 +0200

    Add Componentclass for Map

--- src/waveedit/gui/SwingGUIController.java
+++ src/waveedit/gui/SwingGUIController.java
@@ -1,41 +1,45 @@
 package waveedit.gui;
 
-import java.awt.Graphics2D;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.event.WindowAdapter;
 
-public class SwingGUIController extends JPanel implements IGuiController{
-	Graphics2D g;
-	JFrame frame = new JFrame();
-	JPanel map = new JPanel();
+import javax.swing.JFrame;
 
-	public SwingGUIController() {
+public class SwingGUIController implements IGuiController {
+    Graphics2D g;
+    JFrame frame = new JFrame();
+    SwingGUIMap map;
 
-	}
+    public SwingGUIController() {
 
-	@Override
-	public void initialize() {
-		frame.setTitle("WaveEdit");
-		frame.setSize(800, 600);
-		frame.getContentPane().setBackground(new Color(250, 230, 200));
-		frame.setResizable(false);
-		frame.setVisible(true);
-		repaint();
-		
+    }
 
-		frame.addWindowListener(new WindowAdapter(){
-		    
-		});
-	}
-	protected void paint() {
-		super.paintComponent(g);
-		g.setColor (Color.red);
-		g.fillRect(500,400,50,50);
-		
-		
-;	}
-	
+    @Override
+    public void initialize() {
+        // Fenster öffnen
+        frame.setTitle("WaveEdit");
+        frame.setSize(800, 600);
+        // TODO Hintergrund zeichnen
+        frame.getContentPane().setBackground(new Color(250, 230, 200));
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+        frame.addWindowListener(new WindowAdapter() {
+
+        });
+        // TODO Karte zeichnen
+        map = new SwingGUIMap();
+        frame.getContentPane().add(map);
+        // TODO Menü zeichnen
+    }
+
+    // protected void paint() {
+    // super.paintComponent(g);
+    // g.setColor (Color.red);
+    // g.fillRect(500,400,50,50);
+    //
+    //
+    // }
 
 }
---
+++ src/waveedit/gui/SwingGUIMap.java
@@ -0,0 +1,17 @@
+package waveedit.gui;
+
+import javax.swing.JRootPane;
+
+public class SwingGUIMap extends JRootPane {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1922691013005376966L;
+
+    public SwingGUIMap() {
+        // TODO Auto-generated constructor stub
+
+    }
+
+}
