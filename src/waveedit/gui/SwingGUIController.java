938c12ef4a474d25f6878b59e13a0bdfce57c87a
Gerogeologie <georgeologie@web.de>
2018-04-07 10:35:42 +0200

    Merge

--- src/waveedit/gui/SwingGUIController.java
+++ src/waveedit/gui/SwingGUIController.java
@@ -1,14 +1,15 @@
 package waveedit.gui;
 
+import java.awt.Graphics2D;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.event.WindowAdapter;
 
-public class SwingGUIController implements IGuiController {
-
+public class SwingGUIController extends JPanel implements IGuiController{
+	Graphics2D g;
 	JFrame frame = new JFrame();
-	JPanel background = new JPanel();
+	JPanel map = new JPanel();
 
 	public SwingGUIController() {
 
@@ -16,13 +17,25 @@ public class SwingGUIController implements IGuiController {
 
 	@Override
 	public void initialize() {
+		frame.setTitle("WaveEdit");
 		frame.setSize(800, 600);
-		frame.getContentPane().setBackground(new Color(150, 130, 80));
-
+		frame.getContentPane().setBackground(new Color(250, 230, 200));
+		frame.setResizable(false);
 		frame.setVisible(true);
+		repaint();
+		
+
 		frame.addWindowListener(new WindowAdapter(){
 		    
 		});
 	}
+	protected void paint() {
+		super.paintComponent(g);
+		g.setColor (Color.red);
+		g.fillRect(500,400,50,50);
+		
+		
+;	}
+	
 
 }
