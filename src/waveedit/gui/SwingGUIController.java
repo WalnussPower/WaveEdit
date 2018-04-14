ac2ed90747fa8726f670a4540a500d0fc3274f72
smoenig <smoenig@fb3.uni-bremen.de>
2018-04-14 16:35:41 +0200

    Ctrl+Shit+F

--- src/waveedit/gui/SwingGUIController.java
+++ src/waveedit/gui/SwingGUIController.java
@@ -6,35 +6,32 @@ import java.awt.event.WindowAdapter;
 import javax.swing.JFrame;
 
 public class SwingGUIController implements IGuiController {
-	Graphics2D g;
-	JFrame frame = new JFrame();
-	SwingGUIMap map;
+    Graphics2D g;
+    JFrame frame = new JFrame();
+    SwingGUIMap map;
 
-	public SwingGUIController() {
+    public SwingGUIController() {
 
-	}
+    }
 
-	@Override
-	public void initialize() {
-		// TODO open Frame
-		frame.setTitle("WaveEdit");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(800, 600);
-		// TODO Draw Background
-		frame.setResizable(false);
-		frame.setVisible(true);
-		
+    @Override
+    public void initialize() {
+        // TODO open Frame
+        frame.setTitle("WaveEdit");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        // TODO Draw Background
+        frame.setResizable(false);
+        frame.setVisible(true);
 
-		frame.addWindowListener(new WindowAdapter() {
+        frame.addWindowListener(new WindowAdapter() {
 
-		});
-		// TODO Karte zeichnen
-		map = new SwingGUIMap();
+        });
+        // TODO Karte zeichnen
+        map = new SwingGUIMap();
 
-		frame.getContentPane().add(map);
-		// TODO Menü zeichnen
-	}
-
-
+        frame.getContentPane().add(map);
+        // TODO Menï¿½ zeichnen
+    }
 
 }
